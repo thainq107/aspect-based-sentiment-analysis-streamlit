883ad50621bb9c74d8c35805a294f79bc95979f1
ThaiNQ <110602193+thainq107@users.noreply.github.com>
2025-02-07 21:07:45 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from transformers import pipeline
+
+token_classifier = pipeline(
+    model="thainq107/abte-restaurants-distilbert-base-uncased", 
+    aggregation_strategy="simple"
+)
+
+classifier = pipeline(
+    model="thainq107/absa-restaurants-distilbert-base-uncased"
+)
+
+def main():
+    st.title('Aspect-based Sentiment Analysis')
+    st.header('Model: DistilBERT. Dataset: SemEval4 Restaurants')
+    text_input = st.text_input("Sentence: ", "The bread is top notch as well")
+    results = token_classifier(test_sentence)
+    sentence_tags = " ".join([result['word'] for result in results])
+    pred_label = classifier(f'{test_sentence} [SEP] {sentence_tags}')
+    st.success(f'Sentence: {text_input} === Term: {sentence_tags} === Sentiment: {pred_label["label"]}')
+
+if __name__ == '__main__':
+     main() 
